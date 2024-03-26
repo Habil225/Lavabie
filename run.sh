2a46c1c4f263affc349c48fff644060015cf9963
Habil225 <86432150+Habil225@users.noreply.github.com>
2024-03-26 20:38:50 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-416a6f6b-1d9c-42a3-b83c-0385d6979abd//)
 
 
 echo "$curl_output" >> Update.tsx
